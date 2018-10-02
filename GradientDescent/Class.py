3dfb4d935b317844ee5218c5c8c03e9342e92fe2
Jorrit <jorrit.van.der.laan@hotmail.com>
2018-10-02 12:32:59 +0200

    Line Search

--- GradientDescent/Class.py
+++ GradientDescent/Class.py
@@ -2,6 +2,7 @@ import sys
 
 import numpy as np
 from mnist import MNIST
+from scipy.optimize import minimize
 #import matplotlib as plt
 
 mndata = MNIST('samples')
@@ -19,10 +20,12 @@ data[:,-1][data[:,-1]==7]=1
 #labels = labels[labels == 3 or labels ==7]
 
 class Gradient_Descent():
-    def __init__(self,data,momentum,decay,newton):
+    temp_gradients =[]
+
+    def __init__(self,data,momentum=False,decay=False,newton=False,line_search=False,conjugate=False):
         self.pixels = 785
         self.data = data
-        self.weights = np.random.normal(scale=0.1,size=self.pixels) #sigma = 0.1, so the argument of the sigmoid function does not too big
+        self.weights = np.random.normal(scale=1,size=self.pixels) #sigma = 0.1, so the argument of the sigmoid function does not too big
         self.N = 12396
         self.probabilities = np.zeros(self.N)
         self.learning_rate = 0.1
@@ -34,10 +37,14 @@ class Gradient_Descent():
         self.decay = decay
         self.newton = newton
         self.startGamma = 2
+        self.line_search = line_search
+        self.conjugate = conjugate
+        if conjugate:
+            self.line_search = True
 
 
     def limit_propbs(self, prob):
-        minValue = 1e-20
+        minValue = 1e-10
         maxValue = 1-minValue
         return min(max(minValue,prob),maxValue)
 
@@ -51,10 +58,10 @@ class Gradient_Descent():
         mapping = np.vectorize(self.sigmoid)
         self.probabilities = mapping(np.dot(self.data[:,:-1],x))
         limitMapping = np.vectorize(self.limit_propbs)
-        self.probabilities = limitMapping(self.probabilities)
+        return limitMapping(self.probabilities)
 
-    def calc_error(self):
-        error = self.data[:,-1]*np.log(self.probabilities) + (1-self.data[:,-1])*np.log(1-self.probabilities)
+    def calc_error(self,probs):
+        error = self.data[:,-1]*np.log(probs) + (1-self.data[:,-1])*np.log(1-probs)
         if self.decay:
             error += (self.weight_decay_rate/(2*self.N))*np.sum(self.weights)**2
         return (-1/self.N)*np.sum(error)
@@ -63,6 +70,14 @@ class Gradient_Descent():
         gradients = 1/self.N*np.dot((self.probabilities-self.data[:,-1]),data[:,:-1])
         if self.decay or self.newton:
             gradients += (self.weight_decay_rate/self.N)*self.weights
+        if self.line_search:
+            self.temp_gradients = gradients
+            minimum = minimize(self.line_search_error, 1).x
+            gradients = gradients * minimum
+            if self.conjugate:
+                beta = np.dot(gradients - self.previous_gradients,gradients)/(np.linalg.norm(self.previous_gradients))**2
+                self.temp_gradients = gradients - beta*self.previous_gradients
+
         return gradients
 
     def calculate_hessian(self):
@@ -73,56 +88,38 @@ class Gradient_Descent():
         if self.momentum:
             self.previous_gradients = self.deltas
             self.deltas = self.learning_rate * self.gradient() + self.momentum_term * self.previous_gradients
+        elif self.conjugate:
+            self.previous_gradients = self.deltas
+            self.deltas = self.gradient()
         elif self.newton:
             self.deltas = np.dot(self.calculate_hessian(),self.gradient())
+        elif self.line_search:
+            self.deltas = self.gradient()
         else:
             self.deltas = self.learning_rate * self.gradient()
         self.weights -= self.deltas
 
-    def find_interval(self):
-        original_gradients = self.gradient()
-        self.calc_prop(self.weights-self.startGamma*original_gradients)
-        start_error = self.calc_error()
-        self.calc_prop(self.weights - (self.startGamma*0.1) * original_gradients)
-
-            if self.calc_error() < start_error:
-                self.search_left(original_gradients)
-            else:
-                self.search_right(original_gradients)
-
-
-    def search_left(self, gradients):
-        lastY = self.startGamma
-        lastError = np.math.inf
-
-        self.calc_prob(self.weights - (self.startGamma*0.5) * gradients)
-        newError = self.calc_error()
-        if newError < lastError
-            lastY = lastY * 0.5
-        else:
-            return lastY
-
-        while newError < lastError:
-            self.calc_prob(self.weights - (self.startGamma * 0.5) * gradients)
-            newError = self.calc_error()
-            if newError < lastError
-                lastY = lastY * 0.5
-            else:
-                return lastY
+    def line_search_error(self,gamma):
+        x = self.calc_prop(self.weights - gamma*self.temp_gradients)
+        return self.calc_error(x)
 
 
     def run_iteration(self):
-        self.calc_prop()
+        self.probabilities = self.calc_prop()
         self.update_weights()
 
 
+
 if __name__ == '__main__':
-    Test = Gradient_Descent(data,False,False,True)
+    Test = Gradient_Descent(data,False,False,False,False,True)
     i = 0
+    # Test.run_iteration()
+    # print(Test.line_search_error(0.1))
+
     while True:
         for x in range(0,1):
             Test.run_iteration()
             i += 1
         print(i)
-        print(Test.calc_error())
+        print(Test.calc_error(Test.probabilities))
 
